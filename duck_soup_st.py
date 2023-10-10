0f829f3a6bdd0d3cb3e414726ef9fc7268426b15
robsca <scalas.roberto@gmail.com>
2023-10-10 11:16:13 +0100

    adding upload features - work in db - wip in folder

--- duck_soup_st.py
+++ duck_soup_st.py
@@ -4,6 +4,7 @@ and translation to the web app format.
 
 '''
 import streamlit as st
+import datetime
 import yaml
 import os
 from features.ai import NLP_opeanai
@@ -153,8 +154,9 @@ class DuckSoup_st:
         icon_docs = 'folder' if not with_db else 'database'
         if self.files != []:
             with st.sidebar:
+                children = [sac.MenuItem('Upload', icon='upload')] + [sac.MenuItem(f'{file}', icon='card-text', tag=f'Tag{i}') for i,file in enumerate(self.files)]
                 menu = sac.menu([
-                    sac.MenuItem('Docs', icon=icon_docs, children=[sac.MenuItem(f'{file}', icon='card-text', tag=f'Tag{i}') for i,file in enumerate(self.files)]),
+                    sac.MenuItem('Docs', icon=icon_docs, children = children),
                     sac.MenuItem('Calendar', icon='calendar'),
                     sac.MenuItem('Settings', icon='gear', children=[
                         sac.MenuItem('AI', icon='robot'),
@@ -181,15 +183,32 @@ class DuckSoup_st:
 
     def text_editor(self, with_db = False):
         self.init_css()
-        c1,c2 = st.tabs([f'Text Editor', f'Markdown Preview'])
-        with c1:
-            text = st_ace(placeholder=self.selected_file, value = self.open_selected_file(with_db), height=500)
-        with c2:
-            css_for_markdown = '''
-                <div class="markdown-text-container">
-                 {{text}}
-            '''
-            st.markdown(css_for_markdown.replace('{{text}}',text), unsafe_allow_html=True)
+        with st.form(key='text_editor'):
+            c1,c2,c3 = st.columns([1,1,1])
+            if c1.form_submit_button('New', use_container_width=True):
+                self.OnNew(with_db)
+                st.experimental_rerun()
+            if c2.form_submit_button('Save',use_container_width=True):
+                self.OnSave(with_db)
+            if c3.form_submit_button('Delete', use_container_width=True):
+                self.OnDelete(with_db)
+                st.experimental_rerun()
+
+            title = self.selected_file.replace('.txt','') if not with_db else self.selected_file    
+            
+            col1, col2 = st.columns([1,1])
+            title = col1.text_input('Title', value=title)
+
+            tab1,tab2 = st.tabs([f'Text Editor', f'Markdown Preview'])
+
+            with tab1:
+                text = st_ace(placeholder=self.selected_file, value = self.open_selected_file(with_db), height=500)
+            with tab2:
+                css_for_markdown = '''
+                    <div class="markdown-text-container">
+                    {{text}}
+                '''
+                st.markdown(css_for_markdown.replace('{{text}}',text), unsafe_allow_html=True)
         self.text = text
 
     def OnNew(self, with_db = False):                     # When the user clicks on the NEW-BUTTON or presses Ctrl+N
@@ -330,6 +349,57 @@ class DuckSoup_st:
         cal = calendar(events=calendar_events, options=calendar_options, custom_css=custom_css)
         st.write(cal)
     
+    def OnUpload(self):
+        self.init_css()
+        txt = st.sidebar.file_uploader('Upload a file', type=['txt'])
+        if txt:
+            text = txt.read()
+            # transform the text into a list of lines
+            text = text.decode('utf-8').split('\n')
+            # get the title
+            title = text[1].replace('#','').strip()
+            # get the emoji
+            emoji = text[0]
+            # get the content
+            content = '\n'.join(text[2:])
+            # get the date
+            date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            # get the last modified
+            last_modified = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            # get the tags
+            tags = ''
+            # insert into the database
+            #st.write(emoji, title, content, date, last_modified, tags)
+
+            with st.form(key='upload'):
+                upload_space_button = st.empty()
+                c1,c2,c3,c4 = st.columns([1,1,1,1])
+                emoji = c1.text_input('Emoji', value=emoji)
+                title = c2.text_input('Title', value=title)
+                date = c3.date_input('Date', value='today')
+                time = c4.time_input('Time', value='now')
+                tab1, tab2 = st.tabs(['Text Editor', 'Markdown Preview'])
+                with tab1:
+                    content = st_ace(placeholder=title, value = content, height=500)
+                with tab2:
+                    css_for_markdown = '''
+                        <div class="markdown-text-container">
+                        {{text}}
+                    '''
+                    st.markdown(css_for_markdown.replace('{{text}}',content), unsafe_allow_html=True)
+
+                date_and_time = datetime.datetime.combine(date, time).strftime("%d/%m/%Y %H:%M:%S")
+                upload_button = upload_space_button.form_submit_button('Upload', use_container_width=True)
+                if upload_button and self.with_db:
+                    self.db.insert(emoji, title, content, date_and_time, last_modified, tags)
+                    st.experimental_rerun()
+                elif upload_button and not self.with_db:
+                    with open(os.path.join(self.home_dir, title + '.txt'), 'w') as f:
+                        f.write(emoji)
+                        f.write('\n' + f"# {title}" + '\n')
+                        f.write(content)
+                    st.experimental_rerun()
+
     def ChatFeatures(self):
 
         # get open_ai_key from the config file
@@ -435,38 +505,33 @@ class DuckSoup_st:
                 sac.ButtonsItem(label='Database', icon='database'),
                 sac.ButtonsItem(label='Folder', icon='folder'),
             ])
-        with_db = True if db_or_file == 'Database' else False
+        self.with_db = True if db_or_file == 'Database' else False
+        with_db = self.with_db
         if db_or_file == 'Database':
             self.get_files_from_database()
         else:
             self.get_files_from_archive()
 
         selected = self.create_file_menu(with_db)
-        commands = ['AI', 'Vault', 'Theme', 'About', 'Calendar']
+        commands = ['AI', 'Vault', 'Theme', 'About', 'Calendar', 'Upload']
         try:
             self.selected_file =  selected if selected in self.files else self.files[0] if selected not in commands else selected
             st.write(self.selected_file)
         except:
             self.selected_file = self.files[0] if self.files != [] else 'No files'
         
-        if selected in commands and selected != 'Calendar':
+        if selected in commands and selected in ['AI', 'Vault', 'Theme', 'About']:
             self.Onsettings()
         elif selected == 'Calendar':
             self.OnCalendar()
-        c1,c2,c3 = st.columns([1,1,1])
-        if c1.button('New', key='new', use_container_width=True):
-            self.OnNew(with_db)
-            st.experimental_rerun()
-        if c2.button('Save', key='save', use_container_width=True):
-            self.OnSave(with_db)
-        if c3.button('Delete', key='delete', use_container_width=True):
-            self.OnDelete(with_db)
-            st.experimental_rerun()
+        elif selected == 'Upload':
+            self.OnUpload()
 
         if selected in self.files:
             self.text_editor(with_db)
-        self.ChatFeatures()
+            
 
+        self.ChatFeatures()
 
 if __name__ == '''__main__''':
     app = DuckSoup_st()
